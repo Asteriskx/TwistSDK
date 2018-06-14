64330fd609078dbfc768aba5398682b330e256d7
Asterisk <kuro5858tube@gmail.com>
2018-06-14 23:45:35 +0900

    removed old many files.

--- Twist/Twist/API/REST/Urls.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Twist.API.REST
-{
-	public class Urls
-	{
-		public string Update { get; set; } = "https://api.twitter.com/1.1/statuses/update.json";
-		public string Upload { get; set; } = "https://upload.twitter.com/1.1/media/upload.json";
-		public string HomeTimeLine { get; set; } = "https://api.twitter.com/1.1/statuses/home_timeline.json";
-	}
-}
